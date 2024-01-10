ec19af1d2e0dc272a28e9e1e7288058d243debfe
bedirlituran <120916975+bedirlituran@users.noreply.github.com>
2024-01-11 01:45:02 +0400

    s

--- components/Reklam.jsx
+++ components/Reklam.jsx
@@ -5,7 +5,7 @@ const Reklam = () => {
   return (
     <>
       <div className="flex flex-col lg:flex-row gap-4 mt-20  w-full p-4  ">
-        <div className="md:flex basis-1/2 justify-center items-center hidden">
+        <div className="md:flex basis-1/2 justify-center items-center ">
           <Image
             src="/image/iptv.webp"
             width="700"
@@ -15,7 +15,7 @@ const Reklam = () => {
             alt=""
           />
         </div>
-        <div className="lg:flex hidden basis-1/2 justify-center items-center flex-col space-y-8">
+        <div className="lg:flex  basis-1/2 justify-center items-center flex-col space-y-8">
           <p className="font-turan text-4xl font-bold text-green-950">Sizi düşünərək hazırladıq</p>
           <h1 className="font-turan text-7xl font-bold text-green-950">IPTV<br></br><span className="font-turan text-7xl font-bold text-red-500 ">+++</span><br></br>İNTERNET</h1>
           <Button/>
--- components/Xidmet.jsx
+++ components/Xidmet.jsx
@@ -6,15 +6,15 @@ import Card4 from "./cards/Card4";
 const Xidmet = () => {
   return (
     <>
-      <div className="flex flex-col lg:flex-col items-center justify-center m-auto bg-gray-300 min-h-[600px] w-full shadow-2xl  ">
-        <h1 className=" text-7xl font-semibold font-turan tracking-wider shadow-2xl rounded-2xl ">
+      <div className="flex flex-col items-center justify-center m-auto bg-gray-300 min-h-[600px] w-full shadow-2xl p-4">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold font-turan tracking-wider mb-2 lg:mb-4">
           Xidmətlərimiz
         </h1>
-        <p className="font-turan font-light text-4xl my-5 ">
+        <p className="font-turan font-light text-2xl sm:text-3xl md:text-4xl my-2 lg:my-5">
           Sayt üzərindən sifariş
         </p>
 
-        <div className="flex flex-col lg:flex-row mt-6 h-60 gap-0">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-4 ">
           <Card2 />
           <Card3 />
           <Card4 />
@@ -25,3 +25,4 @@ const Xidmet = () => {
 };
 
 export default Xidmet;
+
