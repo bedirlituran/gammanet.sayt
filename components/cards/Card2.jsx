36719f7b7eafeb07df712b803e5f0d4768782539
bedirlituran <120916975+bedirlituran@users.noreply.github.com>
2024-01-11 02:01:13 +0400

    s

--- components/cards/Card2.jsx
+++ components/cards/Card2.jsx
@@ -12,15 +12,15 @@ const Card2 = () => {
         <Image
           src="/image/xidmet1.png"
           width={400}
-          height={500}
+          height={400}
           alt=""
           objectFit="cover"
           className="rounded-xl group-hover:scale-105  ease-in-out opacity-50 hover:opacity-100 transition duration-500"
         />
       </div>
       <button
-        className="bg-sky-500 hover:bg-sky-700 hover:text-white p-3 w-36 h-16 rounded-2xl hidden  font-turan tracking-wider
-text-lg  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-bold  z-50 group-hover:block group-hover:text-white  transition-all duration-500 shadow-2xl">
+        className="bg-sky-500 hover:bg-sky-700 hover:text-white p-3 w-36 h-16 rounded-2xl invisible  font-turan tracking-wider
+text-lg  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-bold  z-10 group-hover:visible group-hover:text-white  transition-all duration-500 shadow-2xl">
         Sifariş Et
       </button>
     </div>
--- components/cards/Card4.jsx
+++ components/cards/Card4.jsx
@@ -11,7 +11,7 @@ const Card4 = () => {
         <Image
           src="/image/xidmet5.png"
           width={400}
-          height={500}
+          height={400}
           alt=""
           objectFit="cover"
           className="rounded-xl group-hover:scale-105  ease-in-out opacity-50 hover:opacity-100 transition duration-500  "
@@ -19,7 +19,7 @@ const Card4 = () => {
       </div>
       <button
         className="bg-sky-500 hover:bg-sky-700 group-hover:text-white p-3 w-36 h-16 rounded-2xl hidden  font-turan tracking-wider
-        text-lg  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-bold  z-50 group-hover:block   transition-all duration-500 shadow-2xl">
+        text-lg  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-bold  z-10 group-hover:block   transition-all duration-500 shadow-2xl">
         Sifariş Et
       </button>
     </div>
