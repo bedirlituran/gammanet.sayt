f6145e0550f345d83a9f645dd918d310db50b1cb
bedirlituran <120916975+bedirlituran@users.noreply.github.com>
2024-01-15 01:42:53 +0400

    son bugun

--- components/Card.jsx
+++ components/Card.jsx
@@ -24,13 +24,13 @@ const Card = ({ icon, stat, orta, asagi,link }) => {
   const ortas = {
     reqemsal: "Reqemsal TV",
     tarif: "Tariflər",
-    gammatv: "GammaNet Tv Player",
+    gammatv: "GammaNet iptv",
     million: "Million ",
   };
 
   const asagilar = {
     gammaip: "GammaNet App",
-    gamma: "GammaNet iptv",
+    gamma: "Iptv endir",
     kanal: "Kanal siyahısı",
     kompaniya: "Kompaniya",
     emanat: "E-manat",
@@ -50,29 +50,29 @@ const Card = ({ icon, stat, orta, asagi,link }) => {
   
 
   return (
-    <div className="-mt-2 p-2 w-[350px] lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 cursor-pointer md:flex md:flex-nowrap rounded-2xl flex flex-col justify-center items-center">
-      <div className="h-[350px] w-[350px] md:w-full md:flex flex justify-center items-center placeholder:first:group rounded-2xl bg-white py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16 hover:bg-blue-400 hover:text-slate-100 transition-all duration-500 shadow-xl">
-        <div className="flex flex-col mx-auto max-w-xs px-8 w-[260px] flex-wrap">
+    <div className="font-turan -mt-2 p-2 w-[350px] lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 cursor-pointer md:flex md:flex-nowrap rounded-2xl flex flex-col justify-center items-center">
+      <div className="h-[350px] w-[350px] md:w-full md:flex flex justify-center items-center placeholder:first:group rounded-2xl bg-gray-200 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16 hover:bg-red-200 hover:text-slate-100 transition-all duration-500 shadow-xl">
+        <div className="flex flex-col gap-3 mx-auto max-w-xs px-8 w-[260px] flex-wrap">
           <div className="flex flex-row gap-7 items-center justify-center">
             {icons[icon]} {/* Icon prop'u kullan */}
             <p className="text-base font-bold text-gray-950 group-hover:text-white duration-500 uppercase">{stats[stat]}</p>
           </div>
           <div className="mt-6 flex items-baseline justify-center gap-x-2">
-            <p className="text-5xl font-normal tracking-tight text-gray-900">
-              <Link href={links[orta]} className="material-symbols-outlined text-xl border-black group-hover:bg-white group-hover:text-blue-500 hover:border-b-2   group-hover:rounded-full transition-all duration-500">
+          
+              <Link href={links[orta]} className="m-auto  rounded-md px-3 py-2 text-center text-lg font-semibold text-black shadow-2xl hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-turan ">
                 {ortas[orta]}
               </Link>
-            </p>
+            
           </div>
           <div className="flex items-baseline justify-center gap-x-2">
-            <p className="text-2xl font-normal tracking-tight text-gray-900">
-              <Link href={links[asagi]} className="material-symbols-outlined text-xl border-black group-hover:bg-white hover:border-b-2 group-hover:text-blue-500 group-hover:rounded-full transition-all duration-500">
+            
+              <Link href={links[asagi]} className="m-auto  rounded-md px-3 py-2 text-center text-lg font-semibold text-black shadow-2xl hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-turan">
                 {asagilar[asagi]}
               </Link>
-            </p>
+            
           </div>
           <div className="mt-20">
-          <Link href="/" className="m-automt-10 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-2xl hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-turan uppercase">Ətralı</Link>
+          <Link href="/" className="m-auto mt-10  rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-2xl hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-turan uppercase">Qoşul</Link>
           </div>
          
         </div>
